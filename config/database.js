f3f10ea977857a10f12cc38e82d018c3a214be4e
Shivani2810 <108538382+Shivani2810@users.noreply.github.com>
2026-03-24 16:36:43 +0530

    Initial commit

---
+++ config/database.js
@@ -0,0 +1,19 @@
+const mongoose = require ("mongoose");
+
+require("dotenv").config();
+
+exports.connect=()=>{
+
+    mongoose.connect(process.env.MONGODB_URL,{
+        useNewUrlParser:true,
+        useUnifiedTopology:true,
+    })
+    .then(console.log("Database connected successfully"))
+    .catch((error)=>{
+        console.log("Database connection issues");
+        console.error(error);
+        process.exit(1);
+
+    });
+   
+};
