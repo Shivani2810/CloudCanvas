106d3ae12b795243ac8e095354264aadf26c2ba6
Shivani2810 <108538382+Shivani2810@users.noreply.github.com>
2026-03-25 14:29:42 +0530

    implement the file upload from local storage

--- controllers/fileUpload.js
+++ controllers/fileUpload.js
@@ -1,12 +1,35 @@
 const File = require("../models/File");
 
-exports.localFileUpload = async(req,res)=>{
-    try{
-        //  fetching the file;
-    }
-    catch{
+exports.localFileUpload = async (req, res) => {
+  try {
+    // fetch file
+    const file = req.files.file;
+    console.log("File is here ->", file);
 
-    }
-     
-}
-  
+    // create path (with extension)
+    let path = __dirname + "/files/" + Date.now() + `.${file.name.split('.').pop()}`;
+
+    // move file
+    file.mv(path, (err) => {
+      if (err) {
+        console.log(err);
+        return res.status(500).json({
+          success: false,
+          message: "File upload failed"
+        });
+      }
+    });
+
+    res.json({
+      success: true,
+      message: "Local file uploaded successfully"
+    });
+
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: "Something went wrong"
+    });
+  }
+};
